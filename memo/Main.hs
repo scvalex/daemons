646883cb895374e9e5c299064824243198f54112
Alexandru Scvortov <scvalex@gmail.com>
2012-07-31 02:11:26 +0100

    [de]serialization pipes

--- memo/Main.hs
+++ memo/Main.hs
@@ -4,7 +4,13 @@ module Main where
 
 import qualified Data.ByteString.Char8 as B
 import Data.Serialize
+import Data.String
+import Control.Pipe
+import Control.Pipe.Binary
+import Control.Monad
+import Control.Monad.Trans.Class
 import GHC.Generics
+import System.Random
 
 data Command = MemoGet B.ByteString
              | MemoPut B.ByteString B.ByteString
@@ -12,12 +18,40 @@ data Command = MemoGet B.ByteString
 
 instance Serialize Command
 
+deserializer :: (Serialize a, Monad m) => Pipe B.ByteString a m ()
+deserializer = loop Nothing Nothing
+  where
+    loop mk mbin = do
+        bin <- maybe await return mbin
+        case (maybe (runGetPartial get) id mk) bin of
+          Fail reason -> fail reason
+          Partial k   -> loop (Just k) Nothing
+          Done c bin' -> do
+              yield c
+              loop Nothing (Just bin')
+
+serializer :: (Serialize a, Monad m) => Pipe a B.ByteString m ()
+serializer = forever $ do
+    x <- await
+    yield (encode x)
+
+memoGenerator :: Int -> Producer Command IO ()
+memoGenerator n = replicateM_ n $ do
+    m <- lift $ randomRIO (1, n)
+    yield (MemoGet (fromString (show m)))
+
+printer :: Consumer Command IO ()
+printer = forever $ do
+    x <- await
+    lift $ print x
+
+testWrite :: Pipeline IO ()
+testWrite = fileWriter "test.bin" <+< serializer <+< memoGenerator 10
+
+testRead :: Pipeline IO ()
+testRead = printer <+< deserializer <+< fileReader "test.bin"
+
 main :: IO ()
 main = do
-    let bin = encode (MemoGet "name")
-        bin_init = B.init bin
-        bin_last = B.singleton (B.last bin)
-    case runGetPartial get bin_init :: Result Command of
-      Fail reason -> putStrLn ("Error: " ++ reason)
-      Done c _    -> print c
-      Partial k   -> print (k bin_last)
+    runPipe testWrite
+    runPipe testRead
