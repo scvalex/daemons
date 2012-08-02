f84ff1c4c89c593e5fd6e90670cdc3544cb623ac
Alexandru Scvortov <scvalex@gmail.com>
2012-08-02 03:20:09 +0100

    make redirection configurable

--- System/Posix/Daemon.hs
+++ System/Posix/Daemon.hs
@@ -9,26 +9,48 @@ module System.Posix.Daemon (
 import Prelude hiding ( FilePath )
 
 import Control.Monad ( when )
+import Data.Default ( Default(..) )
+import Filesystem.Path.CurrentOS ( FilePath, encodeString )
 import System.Directory ( doesFileExist )
 import System.IO ( SeekMode(..) )
 import System.Posix.IO ( openFd, OpenMode(..), defaultFileFlags, closeFd
                        , dupTo, stdInput, stdOutput, stdError, getLock
                        , LockRequest (..), createFile, setLock, fdWrite )
 import System.Posix.Process ( getProcessID, forkProcess, createSession )
-import Filesystem.Path.CurrentOS ( FilePath, encodeString )
+
+-- | Where should the output (and input) of a daemon be redirected to?
+-- (we can't just leave it to the current terminal, because it may be
+-- closed, and that would kill the daemon).
+--
+-- When in doubt, just use @def@, the default value.
+--
+-- @DevNull@ causes the output to be redirected to @/dev/null@.  This
+-- is safe and is what you want in most cases.
+--
+-- If you don't want to lose the output (maybe because you're using it
+-- for logging), use @ToFile@, instead.
+data Redirection = DevNull
+                 | ToFile FilePath
+                   deriving ( Show )
+
+instance Default Redirection where
+    def = DevNull
 
 -- | Double-fork to create a well behaved daemon.  If @pidfile@ is
 -- given, check/set pidfile; if we cannot obtain a lock on the file,
--- another process is already using it, so fail.
+-- another process is already using it, so fail.  The @redirection@
+-- parameter controls what to do with the standard channels (@stdin@,
+-- @stderr@, and @stdout@).
 --
 -- See: <http://www.enderunix.org/docs/eng/daemon.php>
 --
 -- Note: All unnecessary fds should be close before calling this.
 -- Otherwise, you get an fd leak.
 startDaemon :: Maybe FilePath  -- ^ pidfile
+            -> Redirection     -- ^ redirection
             -> IO ()           -- ^ program
             -> IO ()
-startDaemon maybePidFile program = do
+startDaemon maybePidFile redirection program = do
     checkPidFile
     -- fork first child
     ignore $ forkProcess $ do
@@ -47,9 +69,12 @@ startDaemon maybePidFile program = do
     ignore act = act >> return ()
 
     remapFds = do
-        devnull <- openFd "/dev/null" ReadOnly Nothing defaultFileFlags
-        mapM_ (dupTo devnull) [stdInput, stdOutput, stdError]
-        closeFd devnull
+        let file = case redirection of
+                     DevNull         -> "/dev/null"
+                     ToFile filepath -> encodeString filepath
+        fd <- openFd file ReadOnly Nothing defaultFileFlags
+        mapM_ (dupTo fd) [stdInput, stdOutput, stdError]
+        closeFd fd
 
     withPidFile act =
         case maybePidFile of
