ac28116f8cab6f50468d3a88e18035077c76182f
Alexandru Scvortov <scvalex@gmail.com>
2012-08-04 19:35:18 +0100

    parse arguments; memo example works

--- Memo.hs
+++ Memo.hs
@@ -1,13 +1,16 @@
-{-# LANGUAGE DeriveGeneric, OverloadedStrings, ScopedTypeVariables #-}
+{-# LANGUAGE DeriveGeneric, OverloadedStrings #-}
 
 module Main where
 
 import Control.Concurrent.MVar
 import qualified Data.ByteString.Char8 as B
+import Data.Char ( toLower )
 import Data.Serialize ( Serialize )
+import Data.String ( fromString )
 import qualified Data.Map as M
 import GHC.Generics
 import qualified Network.Socket as NS
+import System.Environment ( getArgs )
 import System.Daemon
 
 data Command = MemoGet B.ByteString
@@ -38,5 +41,11 @@ main :: IO ()
 main = NS.withSocketsDo $ do
     bookVar <- newMVar M.empty
     startDaemon "memo" 7856 (runMemoCommand bookVar)
-    res <- runClient "localhost"  7856 (MemoPut "name" "alex")
+
+    args <- getArgs
+    let args' = map (fromString . map toLower) args
+    res <- case args' of
+      ["get", key]        -> runClient "localhost"  7856 (MemoGet key)
+      ["put", key, value] -> runClient "localhost"  7856 (MemoPut key value)
+      _                   -> error "invalid command"
     print (res :: Maybe Response)
