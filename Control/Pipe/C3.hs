8452117eaf3fc0919d59fe021815b8a1ef131d3c
Alexandru Scvortov <scvalex@gmail.com>
2012-08-04 15:49:08 +0100

    a sprinkling of comments

--- Control/Pipe/C3.hs
+++ Control/Pipe/C3.hs
@@ -10,6 +10,9 @@ import Control.Pipe.Serialize ( serializer, deserializer )
 import Control.Pipe.Socket ( Handler )
 import Data.Serialize ( Serialize )
 
+-- Send a single command over the outgoing pipe and wait for a
+-- response.  If the incoming pipe is closed before a response
+-- arrives, returns @Nothing@.
 commandSender :: (Serialize a, Serialize b) => a -> Handler (Maybe b)
 commandSender command reader writer = do
     runPipe (writer <+< serializer <+< sendCommand)
@@ -24,6 +27,8 @@ commandSender command reader writer = do
         res <- await
         return (Just res)
 
+-- Wait for commands on the incoming pipe, handle them, and send the
+-- reponses over the outgoing pipe.
 commandReceiver :: (Serialize a, Serialize b) => (a -> IO b) -> Handler ()
 commandReceiver executeCommand reader writer = do
     runPipe (writer <+< serializer
