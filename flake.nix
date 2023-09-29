fd65c5490fd992443196a35b9bd48d26003a7a20
Alexandru Scvortov <code@scvalex.net>
2023-09-29 11:25:42 +0100

    build: add nix flake with ghc-9.4

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    utils.url = "github:numtide/flake-utils";
+    flake-compat = {
+      url = github:edolstra/flake-compat;
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, utils, ... }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell = with pkgs; mkShell {
+          buildInputs = [
+            cabal-install
+            tokei
+
+            haskell.compiler.ghc94
+            haskellPackages.pipes
+          ];
+          GIT_EXTERNAL_DIFF = "${difftastic}/bin/difft";
+        };
+      });
+}
